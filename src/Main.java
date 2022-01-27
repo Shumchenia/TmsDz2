b001127df71d631c10e4cd834c4aa1efc2c92743
Matvei Shumchenia <shumchenia.mat@gmail.com>
2022-01-27 19:38:48 +0300

    Add cikl

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,24 @@ public class Main {
     public static void main(String[] args) {
         Integer i = 0;
         while (i < 100) {
-            i++;
-            if (i % 2 == 0) {
+            if (i % 4 != 0) {
+                i++;
                 continue;
             }
-            System.out.println(i - 1);
+            System.out.print((i++) + "  ");
+        }
+        System.out.println();
+        do{
+            i--;
+            if(i%4==0) {
+                System.out.print(i + "  ");
+            }
+        }while(i>=0);
+        System.out.println();
+        for(;i<100;i++){
+            if(i%4!=0){
+                System.out.print(i + "  ");
+            }
         }
     }
 }
